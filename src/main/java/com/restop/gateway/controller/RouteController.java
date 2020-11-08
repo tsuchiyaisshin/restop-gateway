92a476ea322b1993e3a7ed34ccdc4b119ab07298
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-11-08 20:23:53 +0900

    test API

---
+++ src/main/java/com/restop/gateway/controller/RouteController.java
@@ -0,0 +1,19 @@
+package com.restop.gateway.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.restop.gateway.service.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouteController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
