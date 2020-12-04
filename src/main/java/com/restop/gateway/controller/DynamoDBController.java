95f4e4f59354b5d48e14a1244a4d6dbf4f169647
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-12-04 22:03:26 +0900

    complete DynamoDB Setting

--- src/main/java/com/restop/gateway/controller/DynamoDBController.java
+++ src/main/java/com/restop/gateway/controller/DynamoDBController.java
@@ -1,6 +1,10 @@
 package com.restop.gateway.controller;
 
+import com.restop.gateway.dtos.UserDTO;
+import com.restop.gateway.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/dynamoDb")
 public class DynamoDBController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/save")
+    public UserDTO insertIntoDynamoDB(@RequestBody UserDTO dto) {
+        return userService.insertIntoDynamoDB(dto);
+    }
 }
--- src/main/java/com/restop/gateway/service/UserService.java
+++ src/main/java/com/restop/gateway/service/UserService.java
@@ -1,6 +1,7 @@
 package com.restop.gateway.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.restop.gateway.dtos.UserDTO;
 import com.restop.gateway.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,19 @@ public class UserService {
     @Autowired
     private DynamoDBMapper mapper;
 
-    public User insertIntoDynamoDB(User user){
+    public UserDTO insertIntoDynamoDB(UserDTO userDTO){
+        User user = new User();
+        user.setUserId(userDTO.getUserId());
+        user.setName(userDTO.getName());
         mapper.save(user);
-        return user;
+        return convertDto(user);
+    }
+
+    public UserDTO convertDto(User user){
+        return new UserDTO(
+                user.getUserId(),
+                user.getName()
+        );
     }
 
 }
