a1388486624c74ebb24b644bc54f040f06ee1638
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-12-07 22:38:22 +0900

    create db update

--- src/main/java/com/restop/gateway/controller/DynamoDBController.java
+++ src/main/java/com/restop/gateway/controller/DynamoDBController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/dynamoDb")
 public class DynamoDBController {
@@ -18,5 +20,10 @@ public class DynamoDBController {
     @PostMapping("/save")
     public UserDTO insertIntoDynamoDB(@RequestBody UserDTO dto) {
         return userService.insertIntoDynamoDB(dto);
+    }
+
+    @PostMapping("/update")
+    public List<UserDTO> updateDynamoDB(@RequestBody UserDTO dto) {
+        return userService.updateDynamoDB(dto);
     }
 }
--- src/main/java/com/restop/gateway/service/UserService.java
+++ src/main/java/com/restop/gateway/service/UserService.java
@@ -1,10 +1,14 @@
 package com.restop.gateway.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.restop.gateway.dtos.UserDTO;
 import com.restop.gateway.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -18,6 +22,24 @@ public class UserService {
         user.setName(userDTO.getName());
         mapper.save(user);
         return convertDto(user);
+    }
+
+    public List<UserDTO> updateDynamoDB(UserDTO userDTO){
+        User user = new User();
+        user.setUserId(userDTO.getUserId());
+        DynamoDBQueryExpression<User> queryExpression = new DynamoDBQueryExpression<User>()
+                .withHashKeyValues(user);
+
+        List<User> users = mapper.query(User.class, queryExpression);
+
+        ArrayList<UserDTO> userDTOS = new ArrayList<UserDTO>();
+        for(User u : users) {
+            u.setName(userDTO.getName());
+            mapper.save(u);
+            userDTOS.add(convertDto(u));
+        }
+
+        return userDTOS;
     }
 
     public UserDTO convertDto(User user){
